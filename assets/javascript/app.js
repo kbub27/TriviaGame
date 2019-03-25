d0d9c383993dc1e31387ce474507c67a0f8ad9a5
kbub27 <kalebmerriman27@gmail.com>
2019-03-24 22:25:43 -0700

    added function to get a question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,28 +8,47 @@
 // SET AN INTERVAL FOR AN IF STATEMENT 
 // QUESTION IS ANSWERED CORRECTLY THEN WE CAN MOVE ON ........
 
+$(document).ready(function () {
+    
 
+var intervalTime = 30000;
 
-
-var trivia = {
-    qSet1: {
+var trivia = [
+    qSet1 = {
         question:'By law,  what is banned in Japanese restaurants?',
-        answers:[]
+        answers:['tipping','farting','kissing'],
+        name:'qSet1'
     },
-    qSet2: {
-        question:'',
-        answers:[]
+    qSet2 = {
+        question:'According to the 2000 census, in the UK, what is now considered a religion?',
+        answers:['Jedi Knight', 'The "Bock"', 'Pagan Elitism'],
+        name:'qSet2'
     },
-    qSet3: {
-        question:'',
-        answers:[]
+    qSet3 = {
+        question:'Forty three percent of women want to try sadomasochism after smelling what?',
+        answers:['Vanilla Extract','Mens sweat', 'Cologne'],
+        name:'qSet3'
     },
-    qSet4: {
-        question:'',
-        answers:[]
+    qSet4 = {
+        question:'What peoples name means "eaters of raw flesh"?',
+        answers:['Scandanavian', 'Navajo', 'Eskimo'],
+        name:'qSet4'
     },
-    qSet5: {
-        question:'',
-        answers:[]
+    qSet5 = {
+        question:'The city of La Paz in Bolivia is safe from fire because of what?',
+        answers:['Altitude', 'Near the ocean', 'Fire is not allowed'],
+        name:'qSet5'
+    }
+];
+
+function getQ() {
+    for (let i = 0; i < trivia.length; i++) {
+        $('.question').append('<p class="' + trivia[i].name + '"' + '>' + trivia[i].question + '</p>')   
     }
 };
+
+
+
+getQ();
+
+});
